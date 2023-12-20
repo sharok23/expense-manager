1c6d8efff85fd335df23d232a44b721b9b05c934
Sharok N <Sharukhahamed23@gmail.com>
2023-12-20 14:43:20 +0530

    Exception status added

--- src/main/java/com/edstem/expensemanager/exception/EntityNotFoundException.java
+++ src/main/java/com/edstem/expensemanager/exception/EntityNotFoundException.java
@@ -1,8 +1,11 @@
 package com.edstem.expensemanager.exception;
 
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Getter
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class EntityNotFoundException extends RuntimeException {
     private final String entity;
     private final Long id;
--- src/main/java/com/edstem/expensemanager/exception/InvalidUserException.java
+++ src/main/java/com/edstem/expensemanager/exception/InvalidUserException.java
@@ -1,8 +1,11 @@
 package com.edstem.expensemanager.exception;
 
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Getter
+@ResponseStatus(HttpStatus.FORBIDDEN)
 public class InvalidUserException extends RuntimeException {
     private final String entity;
 
